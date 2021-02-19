1ed3430d48f255dbeecb29aae4695bb20e6471bf
Yashwanth <yashwanthdamera6@gmail.com>
2021-02-18 20:08:26 -0600

    Treasure location created

---
+++ location.js
@@ -0,0 +1,28 @@
+
+
+export default [
+    {
+        name: 'Wabash Park',
+        type: 'square',
+        coordinates: [
+            {
+                latitude: 40.353430,
+                longitude: -94.876288
+            },
+            {
+                latitude: 40.353401,
+                longitude: -94.875169
+            },
+            {
+                latitude: 40.354268,
+                longitude: -94.875115
+            },
+            {
+                latitude: 40.354300,
+                longitude: -94.876273
+            }
+          
+        ]
+    }
+
+];
--- scripts/location-square.js
+++ scripts/location-square.js
@@ -1,1 +1,10 @@
 
+export default [
+  
+
+  {
+    latitude: 40.3589695,
+    Longitude: -94.8831951
+}
+
+];
