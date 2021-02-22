57861893847936f8f3cadb50d94af5b182ddd338
Vishal Reddy <59984658+Vishalreddy114@users.noreply.github.com>
2021-02-22 09:09:12 -0600

    Update location-square-test.js

--- scripts/location-square-test.js
+++ scripts/location-square-test.js
@@ -1,1 +1,35 @@
+import isValidCoordinates from './main.js';
 
+
+//negative test case that checks when the given co-ordinates are null 
+// and should return false.
+
+// describe('isValidCoordinates function', () => {
+//     it('checks whether the coordinates are valid', () => {
+//       expect(isValidCoordinates({})).toEqual(false);
+//     });
+//   });
+
+// const isDeviceValid = (dev) => {
+//   // check if dev.coordinate
+//    // check dev.coordinate with isCoordinateValid
+//   return true;
+// }
+// const isLocationValid = (loc) => {
+//   // check if loc.coordinates array exists
+//   // check if loc.coordinates array length is correct for shape
+//   // check each coordinate with isCoordinateValid
+//   return true;
+// }
+// const isInsideQuad = (device, location) => {
+//   // check args
+//   const isDeviceValid = isDeviceValid(device);
+//   const isLocationValid = isLocationValid(location);
+//   if (!isDeviceValid) {
+//     throw new Error('Invalid device');
+//   }
+//   if (!isLocationValid) {
+//     throw new Error('Invalid location');
+//   }
+//   return true;
+// };
